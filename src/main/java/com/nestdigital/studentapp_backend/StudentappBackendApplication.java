f65d0f83e437d2e1153a248fa04e9946eb7d4c47
Sayanaann <alexsayana@gmail.com>
2022-12-05 14:29:33 +0530

    student

---
+++ src/main/java/com/nestdigital/studentapp_backend/StudentappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.studentapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentappBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nestdigital/studentapp_backend/controller/studentController.java
@@ -0,0 +1,15 @@
+package com.nestdigital.studentapp_backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class studentController {
+    @GetMapping("/")
+
+    public String Homepage(){
+        return "welcome to my website";
+
+    }
+}
